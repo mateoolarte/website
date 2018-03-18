5c30c2d1d9fb219be5c6ba4176b6b29378636e9c
Mateo Olarte <mateo.olarte8@gmail.com>
2018-03-18 15:32:44 -0500

    [add] coming soon page and contact links

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react"
-import logo from "../images/logo.svg"
+import github from "../images/github.svg"
+import twitter from "../images/twitter.svg"
+import email from "../images/email.svg"
 import "../styles/App.css"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h1 className="title">Coming soon</h1>
+        <h3 className="subtitle">Contact me:</h3>
+        <div className="social-icons">
+          <a href="https://github.com/mateoolarte" target="_blank" rel="noopener noreferrer"><img src={github} alt=""/></a>
+          <a href="https://twitter.com/mateo_olarte" target="_blank" rel="noopener noreferrer"><img src={twitter} alt=""/></a>
+          <a href="mailto:mateo.olarte8@gmail.com" target="_blank" rel="noopener noreferrer"><img src={email} alt=""/></a>
+        </div>
       </div>
     )
   }
