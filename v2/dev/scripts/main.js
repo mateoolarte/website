02ee4368e460327fdf8eade5257b80efaadfeade
Mateo Olarte <mateo.olarte8@gmail.com>
2018-04-21 19:24:18 -0500

    Updates

--- v2/dev/scripts/main.js
+++ v2/dev/scripts/main.js
@@ -2,11 +2,17 @@ const toggleLandscape = () => {
   const orientation = window.screen.orientation.type;
   let $body = document.body;
 
-  if(orientation == 'landscape-primary' || orientation == 'landscape-secondary') {
+  if (
+    orientation === 'landscape-primary' ||
+    orientation === 'landscape-secondary'
+  ) {
     $body.classList.add('landscape');
-  } else if (orientation == 'portrait-primary' || orientation == 'portrait-secondary') {
+  } else if (
+    orientation === 'portrait-primary' ||
+    orientation === 'portrait-secondary'
+  ) {
     $body.classList.remove('landscape');
   }
-}
+};
 
 window.addEventListener('orientationchange', toggleLandscape);
