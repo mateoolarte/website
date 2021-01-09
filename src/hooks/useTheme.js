caf6d42e5890834aeaccbe079dfedf74af721817
Mateo Olarte <mateo.olarte8@gmail.com>
2021-01-09 00:23:07 -0500

    update libs and useTheme hook

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -2,18 +2,18 @@ import { isWindow } from "../utils/checkWindow";
 
 export default function useTheme() {
   if (isWindow()) {
-    const getSystemMode = window.matchMedia("(prefers-color-scheme: dark)");
     const currentMode = window.localStorage.getItem("theme-mode");
 
     if (currentMode) {
       return currentMode;
     } else {
+      const getSystemMode = window.matchMedia("(prefers-color-scheme: dark)");
       const themeMode = getSystemMode.matches ? "dark" : "light";
       window.localStorage.setItem("theme-mode", themeMode);
 
       return themeMode;
     }
-  } else {
-    return "dark";
   }
+
+  return "dark";
 }
