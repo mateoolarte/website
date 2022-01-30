76e792c87591ce3caa995ec63c32a65379af3c00
Mateo Olarte <mateo.olarte8@gmail.com>
2022-01-30 18:51:23 -0500

    enable GA and update id

--- src/components/withTracker.js
+++ src/components/withTracker.js
@@ -1,31 +1,31 @@
 import React, { Component } from "react";
-// import GoogleAnalytics from "react-ga";
+import GoogleAnalytics from "react-ga";
 
-// GoogleAnalytics.initialize("UA-60678906-1");
+GoogleAnalytics.initialize("G-WD6P5KRG2J");
 
 export default function withTracker(WrappedComponent, options = {}) {
-  // const trackPage = page => {
-  //   GoogleAnalytics.set({
-  //     page,
-  //     ...options
-  //   });
-  //   GoogleAnalytics.pageview(page);
-  // };
+  const trackPage = (page) => {
+    GoogleAnalytics.set({
+      page,
+      ...options,
+    });
+    GoogleAnalytics.pageview(page);
+  };
 
   const HOC = class extends Component {
-    // componentDidMount() {
-    //   const page = this.props.location.pathname;
-    //   trackPage(page);
-    // }
+    componentDidMount() {
+      const page = this.props.location.pathname;
+      trackPage(page);
+    }
 
-    // componentWillReceiveProps(nextProps) {
-    //   const currentPage = this.props.location.pathname;
-    //   const nextPage = nextProps.location.pathname;
+    componentWillReceiveProps(nextProps) {
+      const currentPage = this.props.location.pathname;
+      const nextPage = nextProps.location.pathname;
 
-    //   if (currentPage !== nextPage) {
-    //     trackPage(nextPage);
-    //   }
-    // }
+      if (currentPage !== nextPage) {
+        trackPage(nextPage);
+      }
+    }
 
     render() {
       return <WrappedComponent {...this.props} />;
