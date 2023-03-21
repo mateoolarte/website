ef06099db2f90f4232f777067bab47c60267c2c4
Mateo Olarte <mateo.olarte8@gmail.com>
2023-03-20 22:54:18 -0500

    clean up roadmap and comment remix import

--- components/Head/Head.tsx
+++ components/Head/Head.tsx
@@ -1,12 +1,12 @@
-import { Links, Meta } from "@remix-run/react";
+// import { Links, Meta } from "@remix-run/react";
 
 export function Head() {
   return (
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
-      <Meta />
-      <Links />
+      {/* <Meta />
+      <Links /> */}
       <link rel="icon" type="image/x-icon" href="/favicon.png" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
