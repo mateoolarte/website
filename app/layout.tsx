7423222976acb3bf0637d025a05f450cf4bcb678
Mateo Olarte <mateo.olarte8@gmail.com>
2025-10-12 18:34:17 -0500

    feat: add basic metadata and favicon image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
+import type { Metadata } from "next";
 import { Reddit_Sans } from "next/font/google";
 
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Mateo Olarte - Software Engineer",
+  description: "Portfolio of Mateo Olarte, a software engineer.",
+};
 
 const font = Reddit_Sans({
   subsets: ["latin"],
