13b8706b11890e1c5541364e113ce16f7993c59c
Mateo Olarte <mateo.olarte8@gmail.com>
2025-10-19 13:38:16 -0500

    feat: add github action for ci

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-import { GithubLogoIcon, LinkedinLogoIcon, EnvelopeIcon } from "@phosphor-icons/react/dist/ssr";
+import {
+  GithubLogoIcon,
+  LinkedinLogoIcon,
+  EnvelopeIcon,
+} from "@phosphor-icons/react/dist/ssr";
 
 import styles from "./page.module.css";
 import Link from "next/link";
@@ -7,7 +11,9 @@ function HomePage() {
   return (
     <div className={styles.wrapper}>
       <header className={styles.header}>
-        <h1 className={styles.title}><strong>Mateo Olarte</strong> Software Engineer</h1>
+        <h1 className={styles.title}>
+          <strong>Mateo Olarte</strong> Software Engineer
+        </h1>
       </header>
       <main className={styles.container}>
         <h2 className={styles.heading}>New website coming soon... ☺️</h2>
@@ -15,13 +21,28 @@ function HomePage() {
           <h3 className={styles.contactTitle}>Contact me on:</h3>
           <ul className={styles.links}>
             <li className={styles.linkItem}>
-              <Link href="https://github.com/mateoolarte" className={styles.link}><GithubLogoIcon /></Link>
+              <Link
+                href="https://github.com/mateoolarte"
+                className={styles.link}
+              >
+                <GithubLogoIcon />
+              </Link>
             </li>
             <li className={styles.linkItem}>
-              <Link href="https://www.linkedin.com/in/mateoolarte/" className={styles.link}><LinkedinLogoIcon /></Link>
+              <Link
+                href="https://www.linkedin.com/in/mateoolarte/"
+                className={styles.link}
+              >
+                <LinkedinLogoIcon />
+              </Link>
             </li>
             <li className={styles.linkItem}>
-              <Link href="mailto:mateo.olarte8@gmail.com?subject=%5Bmateoolarte.com%5D" className={styles.link}><EnvelopeIcon /></Link>
+              <Link
+                href="mailto:mateo.olarte8@gmail.com?subject=%5Bmateoolarte.com%5D"
+                className={styles.link}
+              >
+                <EnvelopeIcon />
+              </Link>
             </li>
           </ul>
         </div>
