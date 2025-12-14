8947faf5e6a6c54a643bf2d8164d8502394b4fa7
Mateo Olarte <mateo.olarte8@gmail.com>
2025-12-14 13:44:03 -0500

    fix: add process.env

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,8 +1,6 @@
 import posthog from "posthog-js";
 
-const { NEXT_PUBLIC_POSTHOG_KEY, NEXT_PUBLIC_POSTHOG_HOST } = process.env;
-
-posthog.init(NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: NEXT_PUBLIC_POSTHOG_HOST,
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   defaults: "2025-11-30",
 });
