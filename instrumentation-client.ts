74e9127ffafb843a20e8ed72f9bd575ebe2f5e99
Mateo Olarte <mateo.olarte8@gmail.com>
2025-12-14 13:12:43 -0500

    feat: update posthog web analytics integration

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,9 +1,8 @@
 import posthog from "posthog-js";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://us.posthog.com",
-  defaults: "2025-05-24",
-  capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
-  debug: process.env.NODE_ENV === "development",
+const { NEXT_PUBLIC_POSTHOG_KEY, NEXT_PUBLIC_POSTHOG_HOST } = process.env;
+
+posthog.init(NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: NEXT_PUBLIC_POSTHOG_HOST,
+  defaults: "2025-11-30",
 });
