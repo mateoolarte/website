20de236afa6fe87fa99d862fb2755d3ae691841e
Mateo Olarte <mateo.olarte8@gmail.com>
2026-01-11 17:51:04 -0500

    fix: vulnerabilities issue and unit test

--- app/[locale]/(homepage)/page.test.tsx
+++ app/[locale]/(homepage)/page.test.tsx
@@ -1,9 +1,17 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
+import { NextIntlClientProvider } from "next-intl";
+
+import enMessages from "../../../messages/en.json";
+
 import HomePage from "./page";
 
 test("HomePage", () => {
-  render(<HomePage />);
+  render(
+    <NextIntlClientProvider locale="en" messages={enMessages}>
+      <HomePage />
+    </NextIntlClientProvider>,
+  );
   expect(
     screen.getByRole("heading", {
       level: 1,
