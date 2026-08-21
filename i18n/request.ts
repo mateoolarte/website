1cb0d35d65e7a11013963d92319922952db5ebc0
Mateo Olarte <mateo.olarte8@gmail.com>
2026-08-20 23:33:32 -0500

    refactor(i18n): replace deprecated requestLocale with next/root-params

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,11 +1,19 @@
 import { getRequestConfig } from "next-intl/server";
 import { hasLocale } from "next-intl";
+import * as rootParams from "next/root-params";
+import { notFound } from "next/navigation";
 
 import { routing } from "./routing";
 
-export default getRequestConfig(async ({ requestLocale }) => {
-  const requested = await requestLocale;
-  const locale = hasLocale(routing.locales, requested) ? requested : routing.defaultLocale;
+export default getRequestConfig(async ({ locale }) => {
+  if (!locale) {
+    const paramValue = await rootParams.locale();
+    if (hasLocale(routing.locales, paramValue)) {
+      locale = paramValue;
+    } else {
+      notFound();
+    }
+  }
 
   return {
     locale,
